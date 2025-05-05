2f5dbfb5ad98fa4b6140d40e934483a9b7e15e30
Bryan Michael S. Delos Santos <bmsdelossantos@philrice.local>
2025-05-05 15:25:43 +0800

    created pre-reg delete module

--- app/Http/Controllers/bmAPIController.php
+++ app/Http/Controllers/bmAPIController.php
@@ -632,7 +632,7 @@ class bmAPIController extends Controller
         $extName = $request->extName;
 
         $pythonPath = 'C://Users//Administrator//AppData//Local//Programs//Python//Python312//python.exe';
-		$pythonPath = 'C://Users//bmsdelossantos//AppData//Local//Programs//Python//Python311//python.exe';
+		// $pythonPath = 'C://Users//bmsdelossantos//AppData//Local//Programs//Python//Python311//python.exe';
 
 		$scriptPath = base_path('app//Http//PyScript//bm//deletePreReg.py');
 
