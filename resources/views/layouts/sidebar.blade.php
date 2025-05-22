9367248b228733b27dedbecf9204f638ce98c8d7
Bryan Michael S. Delos Santos <bmsdelossantos@philrice.local>
2025-05-22 09:52:32 +0800

    updated sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1812,10 +1812,7 @@ if($_SERVER['REQUEST_URI']!="/rcef_ds2024/pre_reg/view_farmer"){
 								<li class="sub_menu"><a href="{{route('deletePreReg')}}">Delete Pre-Registration Data</a></li> 
 								
 								
-								
-								
-								@elseif(Auth::user()->username == "dra.jardinez")
-								<li class="sub_menu"><a href="{{route('customExportUI')}}">Custom Export UI</a></li> 
+			
 								@elseif(Auth::user()->username == "NUEVAECIJA_Jhoemar")
 								<li class="sub_menu"><a href="{{route('addSeedGrower')}}">Add Seed Grower</a></li>
 								@endif
