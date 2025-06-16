25ebf2acb55096645f581fde6f5f0e55bd892aa9
Bryan Michael S. Delos Santos <bmsdelossantos@philrice.local>
2025-06-16 10:43:37 +0800

    updated home address online encoding

--- app/Http/Controllers/virtual_encodingController.php
+++ app/Http/Controllers/virtual_encodingController.php
@@ -270,7 +270,7 @@ class virtual_encodingController extends Controller
             ->orderBy("region_sort", 'ASC')
             ->get();
         }else{
-            $provinceList = ["LEYTE","NEGROS OCCIDENTAL","BULACAN"];
+            $provinceList = ["LEYTE","NEGROS OCCIDENTAL","BULACAN","NUEVA ECIJA"];
             $user_provinces = DB::table($GLOBALS['season_prefix'].'rcep_delivery_inspection.lib_prv')
             ->select('lib_prv.province')
             ->whereIn("province", $provinceList)
