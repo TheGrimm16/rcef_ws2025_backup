3ee759b656de8d818d67cd5fec4c9e1d59a8f77a
Bryan Michael S. Delos Santos <bmsdelossantos@philrice.local>
2025-07-03 10:33:16 +0800

    updated virtual encoding controller

--- app/Http/Controllers/virtual_encodingController.php
+++ app/Http/Controllers/virtual_encodingController.php
@@ -270,7 +270,7 @@ class virtual_encodingController extends Controller
             ->orderBy("region_sort", 'ASC')
             ->get();
         }else{
-            $provinceList = ["LEYTE","NEGROS OCCIDENTAL","BULACAN","NUEVA ECIJA","AURORA","BATAAN","PAMPANGA","TARLAC","ZAMBALES","LAGUNA"];
+            $provinceList = ["LEYTE","NEGROS OCCIDENTAL","BULACAN","NUEVA ECIJA","AURORA","BATAAN","PAMPANGA","TARLAC","ZAMBALES","LAGUNA","LA UNION"];
             $user_provinces = DB::table($GLOBALS['season_prefix'].'rcep_delivery_inspection.lib_prv')
             ->select('lib_prv.province')
             ->whereIn("province", $provinceList)
