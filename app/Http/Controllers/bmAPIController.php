7aacde19b820d8d02b65081ff832dd75b57641b7
Bryan Michael S. Delos Santos <bmsdelossantos@philrice.local>
2025-09-18 11:40:38 +0800

    testing

--- app/Http/Controllers/bmAPIController.php
+++ app/Http/Controllers/bmAPIController.php
@@ -833,4 +833,18 @@ class bmAPIController extends Controller
 
     }
 
+    public function getFARs()
+    {
+        $path = base_path('../../rcef_unique_checker/public/rcef_id_generator/public/FLSAR');
+        // $path = realpath($path); // normalize to C:\xampp\rcef_unique_checker\public\rcef_id_generator\public\FLSAR
+
+        if (!$path || !is_dir($path)) {
+            dd("Folder not found: " . $path);
+        }
+
+        $files = scandir($path);
+        dd($files);
+    }
+
+
 }
